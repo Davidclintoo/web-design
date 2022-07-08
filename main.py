dfa6462601abaff9cb02d66e96ae057ff5ccdd75
david onyango <clintoodavi01@gmail.com>
2022-07-08 13:34:00 +0300

    latest

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import re
 
 
 app=Flask(__name__)
-conn=psycopg2.connect("dbname='d1m8odf2nbe0jt' user='idlxsyofckzrsu' port='5432 ' host='ec2-52-30-133-191.eu-west-1.compute.amazonaws.com' password='377cc0aab4454edd009635c4786b072f4e75ef0d07fc222ff7020a6c6d950a4a'")
-app.config['SECRET_KEY'] = 'clintoo1111david444'
-# app.config['SECRET_KEY'] = 'clintoo333david0000'
-# conn=psycopg2.connect("dbname='duka' user='postgres' host='localhost' password='5132'")
+#conn=psycopg2.connect("dbname='d1m8odf2nbe0jt' user='idlxsyofckzrsu' port='5432 ' host='ec2-52-30-133-191.eu-west-1.compute.amazonaws.com' password='377cc0aab4454edd009635c4786b072f4e75ef0d07fc222ff7020a6c6d950a4a'")
+#app.config['SECRET_KEY'] = 'clintoo1111david444'
+app.config['SECRET_KEY'] = 'clintoo333david0000'
+conn=psycopg2.connect("dbname='duka' user='postgres' host='localhost' password='5132'")
 
 
 
@@ -37,10 +37,34 @@ def achievement():
 def about():
     return render_template("about.html") 
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")     
+    
 
+
+
+@app.route("/contact", methods=['GET','POST']) 
+def contact():
+    ms=''
+    if request.method=='post'and 'name' in request.form and 'subject' in request.form and 'email' in request.form and 'message' in request.form:
+       cur=conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+       name=request.form['name']
+       subject=request.form['subject'],
+       email=request.form['email'],
+       message=request.form['message']
+
+       print("message")
+
+       rows=[name,subject,email,message]
+       query=("INSERT INTO public.visions( name, subject, email, message) VALUES (%s, %s,%s,%s)")
+       cur.execute(rows, query)
+
+       conn.commit()
+
+       print('hello')
+      
+      
+          
+    return render_template ('contact.html' , ms=ms )
 
 
 if __name__ == '__main__':
